7c5a2d8672a777be73c19c6b82263243fd488eb0
Moroti Akim <moroti18@gmail.com>
2021-05-09 18:05:23 +0100

    Add autoload, dotenv-loader and retreive DB info for connection

--- inc/includes.php
+++ inc/includes.php
@@ -1,5 +1,24 @@
 <?php
 require "inc/Classes.php";
 require "inc/an-db.php";
-$anch = new Ancherize_DB('ancherize$khaled@paul');
+// Require composer autoloader
+require "vendor/autoload.php";
+require "inc/dotenv-loader.php"
+?>
+<?php
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$driver = $url["scheme"];
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+if(!isset($url["port"])){
+  $dsn = "${driver}:host=${server};dbname=${db}";
+}else{
+  $dsn = "${driver}:host=${server};port={$url['port']};dbname=${db}";
+}
+
+$anch = new Ancherize_DB('ancherize$khaled@paul', $dsn, $username, $password);
 ?>
